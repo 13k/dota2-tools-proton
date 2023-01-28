2f9d3c77146a849ce8b59e63a42ec8fcc0d10b1a
13k <kbmurata@gmail.com>
2023-01-28 20:17:42 -0300

    refactor(build): use spec file with pyinstaller

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,12 +5,15 @@ set -o pipefail
 
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-ROOT_DIR="$(realpath "${SCRIPT_DIR}/..")"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 
-\cd "$ROOT_DIR"
-\pwd
-\poetry install
-\poetry run pyinstaller -y -F --name="d2tp" "d2tp/__main__.py"
-\cp "dist/d2tp" "${D2TP_BIN:?}"
-\chmod 755 "$D2TP_BIN"
+cd "$ROOT_DIR"
+pwd
+
+poetry install
+poetry run pyinstaller "d2tp.spec"
+
+cp "dist/d2tp" "${D2TP_BIN:?}"
+chmod 755 "$D2TP_BIN"
+
 "$D2TP_BIN" --version
